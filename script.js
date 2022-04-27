534f8b3c0f12f3eb6c503bd30dd47c761c94b8d3
Tyler Haven <ty@slh1.com>
2022-04-26 22:05:42 -0600

    Generate computer selection

---
+++ script.js
@@ -0,0 +1,48 @@
+/* Algorithm
+
+Generate computer input
+    math.random 0 - 2
+    assign 0-2 to rock, paper, scissors
+    store in variable
+
+Capture user input
+    store in variable
+
+Compare user to computer
+
+    if user = rock
+        if comp = rock
+            tie
+        if comp = paper
+            comp win
+        if comp = scissors
+            user win
+    
+    if user = scissors
+        if comp = rock
+            user win
+        if comp = paper
+            comp win
+        if comp = scissors
+            tie
+
+    if user = paper
+        if comp = rock
+            user win
+        if comp = paper
+            tie
+        if comp = scissors
+            comp win
+*/
+
+function compInput() {
+    let comp = Math.floor(Math.random() * 3)
+    if (comp === 0) {
+        return 'Rock'
+    } else if (comp === 1) {
+        return 'Paper'
+    } else {
+        return 'Scissors'
+    }
+}
+
