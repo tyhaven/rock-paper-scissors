7f316a9617faf8297d903fb52c4f79b6633297f6
Tyler Haven <ty@slh1.com>
2022-05-02 15:30:40 -0600

    Revert  back to main

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
+/* Algorithm
+Generate computer input
+    math.random 0 - 2
+    assign 0-2 to rock, paper, scissors
+    store in variable
+Capture user input
+    store in variable
+Compare user to computer
+    if user = rock
+        if comp = rock
+            tie
+        if comp = paper
+            comp win
+        if comp = scissors
+            user win
+    
+    if user = scissors
+        if comp = rock
+            user win
+        if comp = paper
+            comp win
+        if comp = scissors
+            tie
+    if user = paper
+        if comp = rock
+            user win
+        if comp = paper
+            tie
+        if comp = scissors
+            comp win
+*/
 
+// function compInput() {
+//     let comp = Math.floor(Math.random() * 3)
+//     if (comp === 0) {
+//         return 'Rock'
+//     } else if (comp === 1) {
+//         return 'Paper'
+//     } else {
+//         return 'Scissors'
+//     }
+// }
 
-
+// function userInput(input) {
+//     return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase()
+// }
 
 function playRound () {
 
@@ -29,7 +72,7 @@ function playRound () {
             //alert('You lose. Try again')
             return 'loss';
         } else if (compInput() === 'Scissors') {
-            // //alert('You win the round!')
+            //alert('You win the round!')
             return 'win';
         }
     } else if (user === 'Paper') {
@@ -59,37 +102,18 @@ function playRound () {
 
 
 function game () {
-    let userWins = 0;
-    let compWins = 0;
-
+    let userWins = [];
     for (let i = 0; i < 5; i++) {
-
-        if (userWins === 3) {
-            alert('You Win')
-            return 'You Win'
-        } else if (compWins === 3) {
-            alert('you lose')
-            return 'You Lose'
-        } else {
-            if (playRound() === 'win') {
-                userWins++
-                console.log(userWins)
-                alert("You win the round!");
-            } else if (playRound() === 'loss'){
-                compWins++
-                console.log(compWins)
-                alert("You didn't win this round");
-            } else {
-                console.log('tie')
-            }
-        }
+         if (playRound() === 'win') {
+             userWins.push('win');
+             alert("You win the round!");
+         } else {
+             alert("You didn't win this round");
+         }  
     }
-
-    if (userWins > compWins) {
+    if (userWins.length >= 2) {
         alert('Congratulations! You win the game!');
-    } else if (compWins > userWins) {
-        alert('Sorry, you lost. Better luck next time');
     } else {
-        alert('It was a tie')
+        alert('Sorry, you lost. Better luck next time');
     }
 }
