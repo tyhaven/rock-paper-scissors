7873fb9f877c84f9f6ef76685569efbd44b870b9
Tyler Haven <ty@slh1.com>
2022-05-04 14:52:34 -0600

    Add count variables, add game win after 5 rounds

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 
+//blank variable to take user selection
 let user = ''
 
     let text = document.querySelector('#result')
+
+    //assign R, P, or S to user variable
     let buttons = document.querySelectorAll('button')
        buttons.forEach(button => { 
         button.addEventListener('click', () => {
@@ -11,9 +14,13 @@ let user = ''
         })
     });
 
+    //Keep track of win count for player and comp
+    let count1 = 0
+    let count2 = 0
 
 function playRound () {
 
+    // generate number and assign it to rock, paper, or scissors
     function compInput() {
         let comp = Math.floor(Math.random() * 3);
         if (comp === 0) {
@@ -25,75 +32,52 @@ function playRound () {
         }
     }
     let compIn = compInput()
-    
+    //Compare user and comp input and return win state for user
     if (user === 'Rock') {
         if (compIn === 'Rock') {
-           // alert('Tie. Try again')
             text.textContent = 'tie';
         } else if (compIn === 'Paper') {
-            //alert('You lose. Try again')
             text.textContent = 'loss';
         } else if (compIn === 'Scissors') {
-            //alert('You win the round!')
             text.textContent = 'win';
         }
     } else if (user === 'Paper') {
         if (compIn === 'Rock') {
-            //alert('You win the round!')
             text.textContent = 'win';
         } else if (compIn === 'Paper') {
-            //alert('Tie. Try again')
             text.textContent = 'tie';
         } else if (compIn === 'Scissors') {
-            //alert('You lose. Try again')
             text.textContent = 'loss';
         }
     } else if (user === 'Scissors') {
         if (compIn === 'Rock') {
-           // alert('You lose. Try again')
            text.textContent = 'loss';
         } else if (compIn === 'Paper') {
-            //alert('You win the round!')
             text.textContent = 'win';
         } else if (compIn === 'Scissors') {
-            //alert('Tie. Try again')
             text.textContent = 'tie';
         }
     }
-}
-
-
-function game () {
-    let userWins = 0
-    let compWins = 0
 
     let player = document.getElementById('player')
+    player.textContent = count1
     let comp = document.getElementById('comp')
-    
-    for (let i = 0; i < 5; i++) {
+    comp.textContent = count2
 
-        if (userWins === 3) {
-            return "You win"
-        } else if (compWins === 3) {
-            return "You lose"
-        } else {
-            let temp = playRound()
-            if (temp === 'win') {
-                console.log(temp)
-                userWins++
-            } else if (temp === 'loss') {
-                console.log(temp)
-                compWins++
-            } else {
-                console.log(temp)
-            }
+    
+        //Cehck if won game, else increase round count by one
+     if(count1 === 5) {
+         text.textContent = "You Win The Game!!!"
+     }  else if (count2 === 5) {
+         text.textContent = 'Sorry, you lost the game:('
+     } else {
+        
+        if (text.textContent === 'tie') {
+            return
+        } else if (text.textContent === 'loss') {
+            comp.textContent = ++count2
+        } else if (text.textContent === 'win') {
+            player.textContent = ++count1
         }
-    }
-    if (userWins > compWins) {
-        alert('Congratulations! You win the game!');
-    } else if (compWins > userWins) {
-        alert('Sorry, you lost. Better luck next time');
-    } else {
-        alert('Tie')
-    }
+     }
 }
